140406a343a1e9c19c12aba4977ae5000ad78d65
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-02 08:17:45 -0700

    Getting data back with axios request

--- components/Tabs.js
+++ components/Tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 // STEP 2: Create tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-api.herokuapp.com/topics
@@ -9,3 +11,21 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+const tabsMaker = () => {
+  const tab = document.createElement("div");
+  tab.classList.add("tab");
+};
+
+const getTabTopics = () => {
+  axios
+    .get("https://lambda-times-api.herokuapp.com/topics")
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+};
+
+getTabTopics();
